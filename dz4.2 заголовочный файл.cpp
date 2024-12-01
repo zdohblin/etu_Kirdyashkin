b6591dd619d8ed035e8959a74911fdc9ffad49e3
zdohblin <2006kirdiashkinmaxim@gmail.com>
2024-12-01 23:52:34 +0300

    Add files via upload

---
+++ dz4.2 заголовочный файл.cpp
@@ -0,0 +1,18 @@
+﻿#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+// Чтение строки из файла по номеру
+void readArcanaInfo(const std::wstring& filename, int arcanaNumber);
+
+// Запись даты в лог
+void logDate(const std::wstring& filename, const std::wstring& date);
+
+// Вывод содержимого лога
+void displayLog(const std::wstring& filename);
+
+// Меню программы
+void showMenu(const std::wstring& infoFile, const std::wstring& logFile);
+
+#endif // FUNCTIONS_H
